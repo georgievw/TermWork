3ba5202693c3c5481d53b5474a39821e5e345169
Georg <georgievw@yandex.ru>
2022-02-07 05:27:38 +0300

    Added simple parser

--- project/news_parser/news_parser/spiders/lenta_spider.py
+++ project/news_parser/news_parser/spiders/lenta_spider.py
@@ -6,16 +6,23 @@ class LentaSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://lenta.ru/parts/news/',
-           # 'http://quotes.toscrape.com/page/1/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
             
-    def parse(self, response):
-        #page = response.url.split("/")[-2]
-        #filename = f'quotes-{page}.html'
-        
-        filename = 'example_lenta.txt'
-        with open(filename, 'wb') as f:
-            f.write(response.body)	
-        self.log(f'Saved file{filename}')
+    def parse(self, response, main=True):
+       if main:
+           for item in response.css('li.parts-page__item'):
+               yield {
+                   'title': item.css('h3::text').get(),
+                   'content': "Пока что пусто",
+                   'published': item.css('div time::text').get(),
+                   'link': response.urljoin(item.css('a').xpath('@href').get()) if item.css('a').xpath('@href').get()[0] == '/' else item.css('a').xpath('@href').get(),
+               }
+           
+               next_page = response.css('li._more a::attr(href)').get()
+               if next_page is not None:
+                   next_page = response.urljoin(next_page)
+                   yield scrapy.Request(next_page, callback=self.parse)
+       else:
+           yield response
