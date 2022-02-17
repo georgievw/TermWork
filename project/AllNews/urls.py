e4bf3900a8bcb8976c34ae36b161df9abe5ba0d5
Georg <georgievw@yandex.ru>
2022-02-17 05:52:03 +0300

    added main interface functure

--- project/AllNews/urls.py
+++ project/AllNews/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import index, by_site
+from .views import index, by_site, by_tags
 
 urlpatterns = [
-    path('<int:site_id>/', by_site),
-    path('', index),
+    path('tags/0/0/', index),
+    path('tags/<int:tag1_id>/<int:tag2_id>/', by_tags),
+    path('sites/<int:site_id>/', by_site),  
+    path('', index), 
+    #path('my_news', my_news)
 ]
--- project/AllNews/views.py
+++ project/AllNews/views.py
@@ -13,11 +13,28 @@ from .models import Article, Site, Tag
 def index(request):
     articles = Article.objects.order_by('-published') 
     sites = Site.objects.all()
-    return render(request, 'index.html', {'articles': articles, 'sites': sites})
+    tags = Tag.objects.all()
+    context = {'articles': articles, 'sites': sites, 'tags': tags}
+    return render(request, 'content.html', context)
     
 def by_site(request, site_id):
     articles = Article.objects.filter(site=site_id)
     sites = Site.objects.all()
-    current_site = Site.objects.get(pk=site_id)
-    context = {'articles': articles, 'sites': sites, 'current_site': current_site}
-    return render(request, 'by_site.html', context)
+    tags = Tag.objects.all()
+    #current_site = Site.objects.get(pk=site_id)
+    context = {'articles': articles, 'sites': sites, 'tags': tags}
+    return render(request, 'content.html', context)
+    
+def by_tags(request, tag1_id, tag2_id):
+    tag1 = Tag.objects.get(pk=tag1_id) if tag1_id != 0 else 0
+    tag2 = Tag.objects.get(pk=tag2_id) if tag2_id != 0 else tag1
+    tag1 = tag2 if tag1_id == 0 else tag1
+    
+    articles1 = tag1.article_set.all().order_by()
+    articles2 = tag2.article_set.all().order_by()
+    articles = articles1 & articles2
+    sites = Site.objects.all()
+    tags = Tag.objects.all()
+    context = {'articles': articles.order_by('-published'), 'sites': sites, 'tags': tags}
+    return render(request, 'content.html', context)
+
